ac55c907ac7dd53ce4ed4cbe24baf20bee1acd3b
Cherian Mathew <cmathew@chakravyu.org>
2016-07-04 22:47:35 +0200

    Update build env to include release versioning

--- project/Common.scala
+++ project/Common.scala
@@ -6,7 +6,6 @@ import sbt._
 object Common {
   lazy val commonSettings = Seq(
     organization := "org.dcs",
-    version := "1.0.0-SNAPSHOT",
     scalaVersion := "2.11.7",
     crossPaths := false,
     checksums in update := Nil,
