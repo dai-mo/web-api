2d6bc015ca4d0a20c74e22d1e33421f21cf6ed4f
Cherian Mathew <cmathew@chakravyu.org>
2016-07-12 18:29:31 +0200

    Add dummy app to check tslint

---
+++ app.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by cmathew on 12/07/16.
+ */
+/**
+ * Created by cmathew on 12/07/16.
+ */
+
+import {Component} from "@angular/core"
+
+@Component({
+    selector: "app",
+    template: "<div>Hello World</div>"
+})
+export class App{}
