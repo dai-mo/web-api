8018e510bd70b185c71beaa9d425e80041a8773d
Cherian Mathew <cmathew@chakravyu.org>
2016-07-20 20:27:13 +0200

    Fix ts lint issues

--- test/assets/javascripts/analyse/flow.template.spec.ts
+++ test/assets/javascripts/analyse/flow.template.spec.ts
@@ -1,11 +1,11 @@
 /**
  * Created by cmathew on 20/07/16.
  */
-import { FlowTemplate } from './flow.template'
+import { FlowTemplate } from "./flow.template"
 
-describe('FlowTemplate', () => {
-    it('has id', () => {
+describe("FlowTemplate", () => {
+    it("has id", () => {
         let ft: FlowTemplate = {id: "12"}
         expect(ft.id).toEqual("12")
     })
-});
+})
