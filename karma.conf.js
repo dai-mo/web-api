514953edf499c305c11b780076b7416da6b0bd4c
Cherian Mathew <cmathew@chakravyu.org>
2016-07-21 14:22:33 +0200

    Add karma test setup

---
+++ karma.conf.js
@@ -0,0 +1,83 @@
+module.exports = function(config) {
+    config.set({
+
+        basePath: './',
+
+        frameworks: ['jasmine'],
+
+        files: [
+            // paths loaded by Karma
+            
+            {pattern: 'node_modules/core-js/client/shim.min.js', included: true, watched: true},
+            {pattern: 'node_modules/zone.js/dist/zone.js', included: true, watched: true},
+
+            {pattern: 'node_modules/reflect-metadata/Reflect.js', included: true, watched: true},
+            {pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
+            {pattern: 'app/assets/javascripts/systemjs.config.js', included: true, watched: true},
+            {pattern: 'karma-test-shim.js', included: true, watched: true},
+
+            // paths loaded via module imports
+            {pattern: 'test/dist/**/*.js', included: false, watched: true},
+            {pattern: 'target/web/public/main/javascripts/**/*.js', included: false, watched: true},
+
+            // third party libs
+            {pattern: 'node_modules/@angular/platform-browser/**/*.js', included: false, watched: false},
+
+            // paths to support debugging with source maps in dev tools
+            {pattern: 'app/assets/**/*.ts', included: false, watched: false},
+            {pattern: 'test/assets/**/*.ts', included: false, watched: false},
+            {pattern: 'test/dist/**/*.js.map', included: false, watched: false}
+        ],
+
+        // proxied base paths
+        proxies: {
+            // required for component assests fetched by Angular's compiler
+            '/test/': '/base/test/',
+            '/assets/lib/': '/base/node_modules/'
+        },
+
+        port: 9876,
+
+        logLevel: config.LOG_INFO,
+
+        colors: true,
+
+        autoWatch: true,
+
+        //browsers: ['Chrome'],
+
+        browsers: ['PhantomJS'],
+
+
+        phantomjsLauncher: {
+            // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+            exitOnResourceError: true
+        },
+
+        // Karma plugins loaded
+        plugins: [
+            'karma-jasmine',
+            'karma-coverage',
+            'karma-chrome-launcher',
+            'karma-phantomjs-launcher',
+            'karma-mocha-reporter'
+        ],
+
+        // Coverage reporter generates the coverage
+        reporters: ['mocha', 'coverage'],
+
+        // Source files that you wanna generate coverage for.
+        // Do not include tests or libraries (these files will be instrumented by Istanbul)
+        preprocessors: {
+            '**/target/web/public/main/javascripts/**/*.js': ['coverage']
+        },
+
+        coverageReporter: {
+            reporters:[
+                {type: 'json', dir: 'test', subdir: 'coverage', file: 'coverage-final.json'}
+            ]
+        },
+
+        singleRun: true
+    })
+};
--- test/assets/javascripts/analyse/flow.template.spec.ts
+++ test/assets/javascripts/analyse/flow.template.spec.ts
@@ -7,6 +7,6 @@ import { FlowTemplate } from "./flow.template"
 describe("FlowTemplate", () => {
     it("has id", () => {
         let ft: FlowTemplate = {id: "12"}
-        expect(ft.id).toEqual("12")
+        expect(ft.id).not.toEqual("12")
     })
 })
