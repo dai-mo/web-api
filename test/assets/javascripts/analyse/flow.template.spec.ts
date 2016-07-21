76857182cbd9d8b0502f0f0dc15db1ff384410fb
Cherian Mathew <cmathew@chakravyu.org>
2016-07-21 09:29:03 +0200

    Fix jasmine typings issue

--- test/assets/javascripts/analyse/flow.template.spec.ts
+++ test/assets/javascripts/analyse/flow.template.spec.ts
@@ -1,6 +1,7 @@
 /**
  * Created by cmathew on 20/07/16.
  */
+/// <reference path="../../../../typings/globals/jasmine/index.d.ts" />
 import { FlowTemplate } from "./flow.template"
 
 describe("FlowTemplate", () => {
